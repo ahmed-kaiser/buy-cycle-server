63117b1b3ebe8e121cf1a3214ec490b32582afb1
Kaiser <ahmedkaiser.001@gmail.com>
2022-11-24 22:08:44 +0600

    setup database connection

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const app = express();
+const port = process.env.POST || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fixmo2v.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const run = async() => {
+
+};
+
+run().catch(err => console.log(err));
+
+
+app.get('/', (req, res) => {
+    res.send("ByCycle server is running");
+});
+
+app.listen(port, () => {
+    console.log(`listening port ${port}`);
+});
