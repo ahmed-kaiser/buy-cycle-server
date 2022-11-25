5afb25ad2dcbbb7a9a870edfb304b0f8ef75bea8
Kaiser <ahmedkaiser.001@gmail.com>
2022-11-25 12:43:59 +0600

    added jwt token verification functionality

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
@@ -11,11 +12,27 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fixmo2v.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+const verifyToken = (req, res, next) => {
+    const token = req.headers?.authorization?.split(' ')[1];
+    if(!token) {
+        res.status(401).send("Unauthorized access");
+    }else{
+        jwt.verify(token, process.env.JWT_TOKEN, (error, decoded) => {
+            if(error) {
+                res.status(401).send("Unauthorized access");
+            }else if(decoded.email !== req.query.email ) {
+                res.status(401).send("Unauthorized access");
+            }else{
+                next();
+            }
+        });
+    }
+};
+
 const run = async() => {
     const userCollections = client.db('BuyCycle').collection('users');
     const categoriesCollections = client.db('BuyCycle').collection('categories');
     const productsCollections = client.db('BuyCycle').collection('products');
-
 
     // add new user data to database
     app.post('/users', async(req, res) => {
@@ -36,7 +53,7 @@ const run = async() => {
         res.send(result);
     });
 
-    app.get('/products', async(req, res) => {
+    app.get('/products', verifyToken, async(req, res) => {
         const query = { ownerEmail: req.query.email }
         const products = await productsCollections.find(query).toArray();
         res.send(products);
@@ -47,11 +64,14 @@ const run = async() => {
         const result = await productsCollections.deleteOne(query);
         res.send(result);
     });
-
 };
 
 run().catch(err => console.log(err));
 
+app.get('/jwt-token', (req, res) => {
+    const token = jwt.sign({email:req.query.email}, process.env.JWT_TOKEN)
+    res.send({token:token});
+});
 
 app.get('/', (req, res) => {
     res.send("ByCycle server is running");
